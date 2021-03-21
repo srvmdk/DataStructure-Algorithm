d1d00f20db48eaf5688e4253154d63d944309cdd
Sourav Modak <mail.souravmodak@gmail.com>
2021-03-21 16:03:19 +0530

    adding push property

--- data-structures/02_doublyLinkedList.js
+++ data-structures/02_doublyLinkedList.js
@@ -16,4 +16,36 @@ class doublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  /* Define push property
+    > Create a new node with the value passed to the function
+    > If the head property is null set the head and tail to be the newly created node 
+    > If not, set the next property on the tail to be that node
+    > Set the previous property on the newly created node to be the tail
+    > Set the tail to be the newly created node
+    > Increment the length
+    > Return the Doubly Linked List
+    */
+  push(val) {
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    }
+
+    this.tail = newNode;
+    this.length++;
+
+    return this;
+  }
 }
+
+// create a doubly linked list
+let list = new doublyLinkedList();
+list.push("Hi!!");
+list.push("this is");
+list.push("Sourav");
+list.push(":)");
