b275987219398763cdb3c821f9b28e80b998aad5
Sourav Modak <mail.souravmodak@gmail.com>
2021-03-21 17:14:14 +0530

    adding pop property

--- data-structures/02_doublyLinkedList.js
+++ data-structures/02_doublyLinkedList.js
@@ -17,6 +17,11 @@ class doublyLinkedList {
     this.length = 0;
   }
 
+  resetHeadTail() {
+    this.head = null;
+    this.tail = null;
+  }
+
   /* Define push property
     > Create a new node with the value passed to the function
     > If the head property is null set the head and tail to be the newly created node 
@@ -40,6 +45,30 @@ class doublyLinkedList {
     this.length++;
 
     return this;
+  }
+
+  /* Define pop property
+    > If there is no head, return undefined
+    > Store the current tail in a variable to return later
+    > If the length is 1, set the head and tail to be null
+    > Update the tail to be the previous Node.
+    > Set the newTail's next to null
+    > Decrement the length
+    > Return the value removed
+    */
+  pop() {
+    if (!this.head) return;
+
+    let removeNode = this.tail;
+    if (this.length === 1) this.resetHeadTail();
+    else {
+      this.tail = removeNode.prev;
+      this.tail.next = null;
+    }
+    this.length--;
+
+    return removeNode.val;
+    list;
   }
 }
 
